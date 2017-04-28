d62a1766b05d86c6ca8314ed9bf9a28b66ca3a0f
AltanCabal <altancabal@gmail.com>
2017-04-28 06:09:11 -0600

    Restore url for requests to the cloud

--- src/app/request.service.ts
+++ src/app/request.service.ts
@@ -5,10 +5,10 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class RequestService {
 
-/*private configEndPoint: string =
-    'http://exploradordeviajes01.us-east-1.elasticbeanstalk.com/browsequotes/v1.0/cr/usd/es-ES/sjo/lax/anytime/anytime';*/
-    private configEndPoint: string =
-    'http://localhost:8080/browsequotes/v1.0/cr/usd/es-ES/sjo/lax/anytime/anytime';
+private configEndPoint: string =
+    'http://exploradordeviajes01.us-east-1.elasticbeanstalk.com/browsequotes/v1.0/cr/usd/es-ES/sjo/lax/anytime/anytime';
+    /*private configEndPoint: string =
+    'http://localhost:8080/browsequotes/v1.0/cr/usd/es-ES/sjo/lax/anytime/anytime';*/
 
   constructor (private http: Http) {  }
 
