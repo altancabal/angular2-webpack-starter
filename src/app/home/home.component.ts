305f17bd396fb234387ab0ce7448171d1487924d
AltanCabal <altancabal@gmail.com>
2017-06-12 06:21:49 -0600

    Add subscription lists for Mexico and Colombia. Change the country order in the main page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,9 +28,9 @@ export class HomeComponent implements OnInit {
   public localState = { value: '' };
 
   public countries = [{name: 'Costa Rica', routerLink: './desde-costa-rica', img:'http://adventuresunlimited.net/wp-content/uploads/2016/07/c1.jpg'},
-                      {name: 'Nicaragua', routerLink: './desde-nicaragua', img:'http://nica-adventures.com/wp-content/uploads/2016/06/relax-travel-nicaragua.jpg'},
-                      {name: 'Mexico', routerLink: './desde-mexico', img:'http://travelhealthpro.org.uk/media_lib/mlib-uploads/full/Mexico.jpg'},
                       {name: 'Colombia', routerLink: './desde-colombia', img:'http://www.enforex.com/img/virtual-tour/colombia/colombia-11.jpg'},
+                      {name: 'Mexico', routerLink: './desde-mexico', img:'http://travelhealthpro.org.uk/media_lib/mlib-uploads/full/Mexico.jpg'},
+                      {name: 'Nicaragua', routerLink: './desde-nicaragua', img:'http://nica-adventures.com/wp-content/uploads/2016/06/relax-travel-nicaragua.jpg'},
                       {name: 'España', routerLink: './desde-espana', img:'https://media-cdn.tripadvisor.com/media/photo-s/03/9b/30/2e/spain.jpg'},
                       {name: 'Argentina', routerLink: './desde-argentina', img:'http://servicesaws.iadb.org/wmsfiles/images/0x0/country-argentina-32882.jpg'},
                       {name: 'Perú', routerLink: './desde-peru', img:'http://www.globeaware.org/images/wide_inside_scrollers/cusco/22-Volunteer%2BVacations%2Bin%2BPeru%2Bwith%2BGlobe%2BAware.jpg'},
@@ -39,7 +39,7 @@ export class HomeComponent implements OnInit {
                       {name: 'Ecuador', routerLink: './desde-ecuador', img:'http://www.panalpina.com/content/www/ecu/en/home/_jcr_content/image.spooler.full.607.jpg/1487933222208.jpg'},
                       {name: 'Guatemala', routerLink: './desde-guatemala', img:'https://www.kfw-entwicklungsbank.de/Bilder/Bilderordner/L%C3%A4nder/Lateinamerika-Karibik/Guatemala-2014-Ruinen_topic_stage.jpg'},
                       {name: 'Cuba', routerLink: './desde-cuba', img:'http://www.airtransat.com/getmedia/0cbe9466-5f8f-4786-8308-4728f5430550/Cuba02.jpg?width=980'},
-                      {name: 'Bolivia', routerLink: './desde-bolivia', img:'http://www.timeforkids.com/files/styles/tfk_rect_large/public/2013-09/bolivia_ss_la-paz.jpg?itok=hhTH6Bzv'},
+                      {name: 'Bolivia', routerLink: './desde-bolivia', img:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Ausangate-hillside-MT.jpg/800px-Ausangate-hillside-MT.jpg'},
                       {name: 'República Dominicana', routerLink: './desde-republica-dominicana', img:'http://www.republica-dominicana-live.com/upload/playa-rincon-republica-dominicana-01.jpg'},
                       {name: 'Honduras', routerLink: './desde-honduras', img:'http://kids.nationalgeographic.com/content/dam/kids/photos/Countries/H-P/honduras-ocean-port.adapt.945.1.jpg'},
                       {name: 'Paraguay', routerLink: './desde-paraguay', img:'http://www.avina.net/avina/wp-content/uploads/2015/11/header-paraguay.jpg'},
